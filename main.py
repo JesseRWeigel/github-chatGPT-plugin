fa81195172e12e0b1ce597b85f2c8c627cac8d81
Jesse Weigel <jesseweigel@gmail.com>
2023-05-06 01:22:25 -0400

    wip: add ability to list files in a repo

--- main.py
+++ main.py
@@ -1,49 +1,41 @@
-import requests
+import httpx
 import quart
 import quart_cors
 from quart import request, jsonify
+import base64
 
 app = quart_cors.cors(quart.Quart(__name__),
                       allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-# _TODOS = {}
 
-# @app.post("/todos/<string:username>")
-# async def add_todo(username):
-#     request = await quart.request.get_json(force=True)
-#     if username not in _TODOS:
-#         _TODOS[username] = []
-#     _TODOS[username].append(request["todo"])
-#     return quart.Response(response='OK', status=200)
-
-
-@app.get("/repo/<string:username>/<string:repo_name>")
+@app.route("/repo/<string:username>/<string:repo_name>", methods=['GET'])
 async def get_repo_data(username, repo_name):
     # Define the URL for the GitHub API endpoint
     url = f"https://api.github.com/repos/{username}/{repo_name}"
 
     # Make an HTTP GET request to the API endpoint
-    response = requests.get(url)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
 
     # Check if the request was successful
     if response.status_code == 200:
         # Parse the JSON response
         repo_data = response.json()
-        return repo_data
+        return jsonify(repo_data)
     else:
         # Handle unsuccessful requests
         print(f"An error occurred: {response.status_code}")
         return None
 
 
-@app.get("/file/<string:owner>/<string:repo>/<string:file_path>")
-async def get_file_data(owner, repo, file_path):
+@app.route("/list-files/<string:owner>/<string:repo>/<string:path>", methods=['GET'])
+async def get_file_data(owner, repo, path):
     # Construct the API URL
-    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{file_path}'
+    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
 
     # Make the GET request to the GitHub API
-    response = requests.get(url)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
 
     # Check if the request was successful
     if response.status_code == 200:
@@ -51,7 +43,6 @@ async def get_file_data(owner, repo, file_path):
         file_data = response.json()
 
         # Decode the file content (which is base64 encoded)
-        import base64
         file_content = base64.b64decode(file_data['content']).decode('utf-8')
 
         # Return the file content as a JSON response
@@ -61,23 +52,37 @@ async def get_file_data(owner, repo, file_path):
         return jsonify({'error': f'Error: {response.status_code}'}), response.status_code
 
 
-# @app.delete("/todos/<string:username>")
-# async def delete_todo(username):
-#     request = await quart.request.get_json(force=True)
-#     todo_idx = request["todo_idx"]
-#     # fail silently, it's a simple plugin
-#     if 0 <= todo_idx < len(_TODOS[username]):
-#         _TODOS[username].pop(todo_idx)
-#     return quart.Response(response='OK', status=200)
+@app.route("/list-files/<string:owner>/<string:repo>/<path:path>", methods=['GET'])
+async def list_files(owner, repo, path=""):
+    # Construct the API URL
+    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+
+    # Make the GET request to the GitHub API
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        contents = response.json()
+
+        # Extract the names of the files and directories
+        file_names = [item["name"] for item in contents]
+
+        # Return the file names as a JSON response
+        return jsonify({'files': file_names})
+    else:
+        # Return an error response with the status code
+        return jsonify({'error': f'Error: {response.status_code}'}), response.status_code
 
 
-@app.get("/logo.png")
+@app.route("/logo.png", methods=['GET'])
 async def plugin_logo():
     filename = 'logo.png'
     return await quart.send_file(filename, mimetype='image/png')
 
 
-@app.get("/.well-known/ai-plugin.json")
+@app.route("/.well-known/ai-plugin.json", methods=['GET'])
 async def plugin_manifest():
     host = request.headers['Host']
     with open("./.well-known/ai-plugin.json") as f:
