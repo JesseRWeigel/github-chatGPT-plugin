f0c7bbd206e845c730ff3f16d08df8ca5db8d643
Jesse Weigel <jesseweigel@gmail.com>
2023-05-08 21:40:09 -0400

    update to use github auth and graphql

--- main.py
+++ main.py
@@ -2,78 +2,50 @@ import httpx
 import quart
 import quart_cors
 from quart import request, jsonify
-import base64
+import os
 
 app = quart_cors.cors(quart.Quart(__name__),
                       allow_origin="https://chat.openai.com")
 
 
-@app.route("/repo/<string:username>/<string:repo_name>", methods=['GET'])
-async def get_repo_data(username, repo_name):
-    # Define the URL for the GitHub API endpoint
-    url = f"https://api.github.com/repos/{username}/{repo_name}"
+@app.route("/graphql", methods=['POST'])
+async def graphql():
+    # Get the JSON data from the request
+    data = await request.get_json()
 
-    # Make an HTTP GET request to the API endpoint
+    # Get the query from the data
+    query = data.get('query')
+
+    # Define the URL for the GitHub GraphQL API endpoint
+    url = "https://api.github.com/graphql"
+
+    # Get the GitHub OAuth token from an environment variable
+    token = os.getenv('GITHUB_TOKEN')
+
+    # Define the headers for the request
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    # Define the payload for the request
+    payload = {
+        "query": query
+    }
+
+    # Make an HTTP POST request to the API endpoint
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
+        response = await client.post(url, headers=headers, json=payload)
 
     # Check if the request was successful
     if response.status_code == 200:
         # Parse the JSON response
-        repo_data = response.json()
-        return jsonify(repo_data)
+        graphql_data = response.json()
+        return jsonify(graphql_data)
     else:
         # Handle unsuccessful requests
         print(f"An error occurred: {response.status_code}")
         return None
-
-
-@app.get("/file/<string:owner>/<string:repo>/<string:file_path>")
-async def get_file_data(owner, repo, file_path):
-    # Construct the API URL
-    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{file_path}'
-
-    # Make the GET request to the GitHub API
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the JSON response
-        file_data = response.json()
-
-        # Decode the file content (which is base64 encoded)
-        import base64
-        file_content = base64.b64decode(file_data['content']).decode('utf-8')
-
-        # Return the file content as a JSON response
-        return jsonify({'content': file_content})
-    else:
-        # Return an error response with the status code
-        return jsonify({'error': f'Error: {response.status_code}'}), response.status_code
-
-
-@app.route("/list-files/<string:owner>/<string:repo>/<path:path>", methods=['GET'])
-async def list_files(owner, repo, path=""):
-    # Construct the API URL
-    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
-
-    # Make the GET request to the GitHub API
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the JSON response
-        contents = response.json()
-
-        # Extract the names of the files and directories
-        file_names = [item["name"] for item in contents]
-
-        # Return the file names as a JSON response
-        return jsonify({'files': file_names})
-    else:
-        # Return an error response with the status code
-        return jsonify({'error': f'Error: {response.status_code}'}), response.status_code
 
 
 @app.route("/logo.png", methods=['GET'])
